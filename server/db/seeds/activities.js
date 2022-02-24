a0a65b0f1bd6c99fb59a217be3ef47ff77417620
Patch Hareton Oliver <patcholiver@hotmail.com>
2022-02-24 16:16:51 +1300

    Build data

---
+++ server/db/seeds/activities.js
@@ -0,0 +1,33 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('activities').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('activities').insert([
+        {id: 1, title: 'Yoga', category: 'Health', info: 'a gentle vinyasa flow to start the day. suitable for all levels', date: 'every thursday at', time: '10am', location: 'blue room', recurring: true, frequency: 'weekly', image: '', ran_by: 'kaia',},
+        {id: 2, title: 'Movie Night', category: 'Fun', info: 'something something cool movie something', date: '', time: '', location: 'green room', recurring: false, frequency: 'n/a', image: '', ran_by: 'finn',},
+        {id: 3, title: 'Te Reo Classes', category: 'Culture', info: 'Come along and learn te reo!', date: 'every monday at', time: '5pm', location: 'pink room', recurring: true, frequency: 'weekly', image: '', ran_by: 'isaiah',},
+      ]);
+    });
+};
+
+
+exports.up = function(knex) {
+  return knex.schema.createTable('activities', (table) => {
+      table.increments('id')
+      table.string('title')
+      table.string('category')
+      table.string('info')
+      table.string('date')
+      table.string('time')
+      table.string('location')
+      table.boolean('recurring')
+      table.string('frequency')
+      table.string('image')
+      table.string('ran_by')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('activities')
+};
