aa27fb3245e01141985e92978a85f4435805cf08
Patch Hareton Oliver <patcholiver@hotmail.com>
2022-02-25 16:50:10 +1300

    Add bootstrap buttons and alerts

--- client/components/Sessions.jsx
+++ client/components/Sessions.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react'
+import Button from "react-bootstrap/Button"
+import Alert from "react-bootstrap/Alert"
 
 import { fetchSessions } from '../apis/api'
 
 function Sessions () {
 
     const [sessions, setSessions] = useState(null)
+    const [showAlert, setShowAlert] = useState(false)
 
     useEffect(() => {
         fetchSessions()
@@ -13,23 +16,31 @@ function Sessions () {
     },[])
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        alert(`Awesome! We'll see you there!`)
+        e.preventDefault();
+        setShowAlert(true)
       }
 
 
     return (
         <> 
 
+        <header className="mt-4 header">
         <h3>Group therapy sessions & workshops</h3>
+        </header>
 
+        <Alert variant="success" show={showAlert} onClose={() => setShowAlert(false)} dismissible>
+        <Alert.Heading>Awesome! We'll see you there!</Alert.Heading>
+        </Alert>
+        
         <ul>
         { sessions ? sessions.map(sesh => {
         return <li key={sesh.id}>
             <b>session: </b><em>{sesh.title + ' // ' + sesh.info}</em>
             <p>{sesh.date} {sesh.time} in {sesh.location}</p>
             <p>ran by: {sesh.ran_by}</p>
-            <button onClick={handleSubmit}>i'm keen!</button>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+            i'm keen!
+            </Button>
             <br></br>
             <br></br>
             </li>
