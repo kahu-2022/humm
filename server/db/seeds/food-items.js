045a2d4640decd43652c65f72e7706f4fc86f2a8
john-cannon <a.john.cannon26@gmail.com>
2022-02-26 15:33:29 +1300

    fixed minor food seed error

--- server/db/seeds/food-items.js
+++ server/db/seeds/food-items.js
@@ -7,8 +7,8 @@ exports.seed = async function(knex) {
     {id: 4, name: 'Tyson', donorRoom: '6', item: 'Tangelo', quantity: '20', donateDate: '2022-02-25', useByDate: '2022-03-04', status: 'Available', claimedBy: '', claimerRoom: ''},
     {id: 5, name: 'Anastacia', donorRoom: '8', item: 'Kumara', quantity: '8', donateDate: '2022-02-22', useByDate: '2022-03-01', status: 'Available', claimedBy: '', claimerRoom: ''},
     {id: 6, name: 'Maria', donorRoom: '9', item: 'Brown rice', quantity: '2 X bags', donateDate: '2022-02-12', useByDate: '2022-02-19', status: 'Claimed', claimedBy: 'Simon', claimerRoom: '13'},
-    {id: 7, name: 'Simon', donorRoom: '13', item: 'Kumquat', quantity: '5', donateDate: '', useByDate: '2022-02-24', status: '2022-03-03', claimedBy: 'Available', claimerRoom: ''},
-    {id: 8, name: 'Kevin', donorRoom: '19', item: 'Persimmon', quantity: '10', donateDate: '', useByDate: '2022-02-24', status: '2022-03-03', claimedBy: 'Available', claimerRoom: ''},
+    {id: 7, name: 'Simon', donorRoom: '13', item: 'Kumquat', quantity: '5', donateDate: '2022-02-24', useByDate: '2022-03-03', status: 'Available', claimedBy: '', claimerRoom: ''},
+    {id: 8, name: 'Kevin', donorRoom: '19', item: 'Persimmon', quantity: '10', donateDate: '2022-02-24', useByDate: '2022-03-03', status: 'Available', claimedBy: '', claimerRoom: ''},
     {id: 9, name: 'Cherie', donorRoom: '20', item: 'Raspberries', quantity: '3 X punnets', donateDate: '2022-02-25', useByDate: '2022-03-04', status: 'Available', claimedBy: '', claimerRoom: ''}
   ]);
 };
