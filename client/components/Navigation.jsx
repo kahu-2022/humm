b2f7e0a837e169d0d0a523bcdd88734393a2b71e
john-cannon <a.john.cannon26@gmail.com>
2022-02-26 15:46:48 +1300

    spelling error fixes

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation(props) {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light  " variant="light  ">
   <Container>
-  <Navbar.Brand href="/">hummm</Navbar.Brand>
+  <Navbar.Brand href="/">humm</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
