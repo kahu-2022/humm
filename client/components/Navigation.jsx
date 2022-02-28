3934ba99cabf547a82210ba03ff38a364dd84e6e
rose-baylis <rose.l.baylis@gmail.com>
2022-03-01 11:30:03 +1300

    Nav items hidden if not logged in

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,35 +1,79 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
-import Nav from "react-bootstrap/Nav"
-import Container from "react-bootstrap/Container"
+import { Navbar, NavDropdown, Nav, Container, Button } from "react-bootstrap"
+// import NavDropdown from "react-bootstrap/NavDropdown"
+// import Nav from "react-bootstrap/Nav"
+// import Container from "react-bootstrap/Container"
+
+import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated"
+
+import { useAuth0 } from "@auth0/auth0-react"
 
 function Navigation(props) {
+  const { loginWithRedirect, logout } = useAuth0()
+
+  // TODO: call the useAuth0 hook and destructure logout and loginWithRedirect
+  function handleLogoff(e) {
+    e.preventDefault()
+    //console.log('log off')
+    logout({ returnTo: window.location.origin })
+  }
+
+  function handleRegister(e) {
+    e.preventDefault()
+    loginWithRedirect({
+      redirectUri: `${window.location.origin}/register`,
+    })
+  }
+
+  function handleSignIn(e) {
+    e.preventDefault()
+    // console.log("sign in")
+    loginWithRedirect()
+  }
+
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="light  " variant="light  ">
-  <Container>
-  <Navbar.Brand href="/">humm</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="about">About us</Nav.Link>
-      <NavDropdown title="Mental Health" id="collapsible-nav-dropdown">
-        <NavDropdown.Item href="/counsellors">Browse counsellors</NavDropdown.Item>
-        <NavDropdown.Item href="/booking">Book counselling</NavDropdown.Item>
-        <NavDropdown.Item href="/sessions">Group Therapy</NavDropdown.Item>
-        <NavDropdown.Item href="/emergencyresources">Emergency Resources</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="/activities">Community Activities</Nav.Link>  
-      <Nav.Link href="/food">Food</Nav.Link>    
-    </Nav>
-    <Nav>
-      <Nav.Link href="/room">Report an issue</Nav.Link>
-      <Nav.Link href="/volunteering">Volunteering</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Container>
+        <Navbar.Brand href="/">humm</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link href="about">About us</Nav.Link>
+          </Nav>
+          <IfAuthenticated>
+            <Nav className="me-auto">
+              <NavDropdown title="Mental Health" id="collapsible-nav-dropdown">
+                <NavDropdown.Item href="/counsellors">
+                  Browse counsellors
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/booking">
+                  Book counselling
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/sessions">
+                  Group Therapy
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/emergencyresources">
+                  Emergency Resources
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/activities">Community Activities</Nav.Link>
+              <Nav.Link href="/food">Food</Nav.Link>
+            </Nav>
+          <Nav>
+            <Nav.Link href="/room">Report an issue</Nav.Link>
+            <Nav.Link href="/volunteering">Volunteering</Nav.Link>
+          </Nav>
+          </IfAuthenticated>          
+          
+        </Navbar.Collapse>
+
+        <IfNotAuthenticated>
+        <Button variant="primary">Login</Button>
+        </IfNotAuthenticated>
+      </Container>
+    </Navbar>
   )
 }
 
--- client/index.js
+++ client/index.js
@@ -1,21 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
+  import React from "react"
+  import ReactDOM from "react-dom"
+  import { BrowserRouter as Router } from "react-router-dom"
 
-import App from "./components/App";
-// import store from "./store";
+  import App from "./components/App"
 
-import "bootstrap/dist/css/bootstrap.min.css";
+  import { Auth0Provider } from "@auth0/auth0-react"
 
+  import "bootstrap/dist/css/bootstrap.min.css"
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    // <Provider store={store}>
-      <Router>
-        <App />
-      </Router>,
-    // </Provider>,
-    document.getElementById("app")
-  );
-});
+  document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(
+      /**
+       * Auth0Provider is a component that has a hook that provides
+       * all authorization operations
+       */
+      <Auth0Provider
+        domain={"kahu-2022-rosebaylis.au.auth0.com"}
+        clientId={"zFvQlxd1lxF5POaM4rS4BITKzm8i98xr "}
+        redirectUri={window.location.origin}
+        audience="https://humm/api"
+      >
+        <Router>
+          <App />
+        </Router>
+      </Auth0Provider>,
+
+      document.getElementById("app")
+    )
+  })
