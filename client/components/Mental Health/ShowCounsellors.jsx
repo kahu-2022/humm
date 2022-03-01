6c13999d6081d14d3d60887bebded76daad8cf68
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-03-01 16:22:58 +1300

    filter counsellors based on speciality

--- client/components/Mental Health/ShowCounsellors.jsx
+++ client/components/Mental Health/ShowCounsellors.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect, useMemo } from 'react'
+import { Container, Row, Col, Form } from 'react-bootstrap'
 import Counsellor from './Counsellor'
 import PageHeader from '../PageHeader'
 
@@ -7,6 +7,17 @@ import { fetchCounsellors } from '../../apis/api'
 
 function ShowCounsellors(props) {
   const [counsellor, setCounsellor] = useState([])
+
+  const categories = ['All', 'Addiction', 'Anxiety & Stress', 'Job Support', 'LGBTQI+', 'Relationships', 'Te Whare Tapa Whā', 'Trauma & Grief']
+
+  const [category, setCategory] = useState('') 
+
+  const filteredData = useMemo(() => {  
+    if (!category || category === "All") return counsellor
+
+    return counsellor.filter(item => item.speciality === category) 
+  }, [category])
+
 
   useEffect(() => {
     getCounsellors()
@@ -24,9 +35,24 @@ function ShowCounsellors(props) {
         title="Our Counsellors"
         description="Meet our team of friendly counselling staff!"
       />
+      
       <Container>
+        <Form.Group
+          className="mb-3"
+          controlId="counsellorSpeciality"
+          key={'e'}
+          onChange={e => setCategory(e.target.value)}
+          >
+            <Form.Label>Counsellor Speciality</Form.Label>
+              <Form.Select name="counsellorSpeciality" aria-label="counsellorSpeciality" >
+                <option>Select Speciality</option>
+                  {categories.map((speciality, index) => {
+                    return (<option value={speciality} key={index} >{speciality}</option>)
+                  })}
+              </Form.Select>
+            </Form.Group>
         <Row className="g-3">
-          {counsellor.map((counsellor) => {
+          {filteredData.map((counsellor) => {
             return (
               <Col md={6} lg={4} key={counsellor.id}>
                 <Counsellor counsellor={counsellor} />
