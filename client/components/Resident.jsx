b7ff895d034142312cdca981956800a1d0c32221
rose-baylis <rose.l.baylis@gmail.com>
2022-03-01 18:16:51 +1300

    add user profile page

--- client/components/Resident.jsx
+++ client/components/Resident.jsx
@@ -1,27 +1,110 @@
-import React from "react"
-import Container from "react-bootstrap/Container"
+import React, {useState} from "react"
+import { Form, Button, Image, Container, Row, Col } from "react-bootstrap"
 
+import PageHeader from "./PageHeader"
+import Loading from "./Loading"
 
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
 
 function Resident() {
+  const { user } = useAuth0()
+
+  const [formData, setFormData] = useState()
+  const handleChange = () =>{
+
+  }
+
+  const handleSubmit = () =>{
+
+  }
   return (
     <>
-    <Container>
-    <header className="mt-4 header">
-    <h1>Hey there! Welcome to your personal page. Here you can see a list of things you've signed up for.</h1>
-    </header>
+      <Container>
+        <Row>
+          <Col md={6}>
+            <PageHeader
+              title="Profile page"
+              description="Feel free to help us out around the place."
+            />
+            <Container>
+              <Image src={user?.picture} roundedCircle={true} />
+              <p className="data-title mb-0 mt-3">Email</p>
+              <p>{user?.email}</p>
+              <span class="border-bottom"></span>
 
-    <section className="main">
-      <br></br>
-      <h4> (data based on login + user interaction w other pages) </h4>
-      <h4> (nag hareton to finish it lol) </h4>
-      <br></br>
-    </section>
+            </Container>
+            <Container>
+          <Form onSubmit={handleSubmit}>
 
-    </Container>
+            <Form.Group
+              className="mb-3"
+              controlId="name"
+              onChange={handleChange}
+              value = {formData?.name}
+            >
+              <Form.Label>Your full name</Form.Label>
+              <Form.Control
+                name="name"
+                type="text"
+                placeholder="Enter your name"
+                
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="pronouns"
+              onChange={handleChange}
+            >
+              <Form.Label>Your Pronouns</Form.Label>
+              <Form.Control
+                name="pronouns"
+                type="text"
+                placeholder="Enter your preferred pronouns"
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="roomNumber"
+              onChange={handleChange}
+            >
+              <Form.Label>Room number</Form.Label>
+              <Form.Control
+                name="roomNumber"
+                type="text"
+                placeholder="Enter your room number"
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="contactDetails"
+              onChange={handleChange}
+              key={"j"}
+            >
+              <Form.Label>Contact Details</Form.Label>
+              <Form.Control
+                name="contactDetails"
+                as="textarea"
+                rows={3}
+                placeholder="Enter how you'd like to be contacted here"
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Save changes
+            </Button>
+            </Form>
+
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </>
-    
   )
 }
 
-export default Resident
+export default withAuthenticationRequired(Resident, {
+  onRedirecting: () => <Loading />,
+})
