d8cd5b7d20b60b2757c544ddbefcbc933f7ea854
rose-baylis <rose.l.baylis@gmail.com>
2022-03-01 21:22:22 +1300

    Tidied up text

--- client/components/Resident.jsx
+++ client/components/Resident.jsx
@@ -47,11 +47,8 @@ function Resident() {
   const handleSubmit = (event) => {
     event.preventDefault()
     if (userExists) {
-      console.log("user exists", formData)
       updateUser(formData)
       .then((updatedUser) => {
-        console.log("returned", updatedUser[0])
-
         setFormData(updatedUser[0])
         setShowAlert(true)
       })
@@ -64,23 +61,23 @@ function Resident() {
   return (
     <>
       <Container className="mt-3"></Container>
-
       <Container>
         <Row>
-          <Col md={{ span: 6, offset: 3 }}>
-            <Alert
+          <Col className = "m-2 styled-form" md={{ span: 6, offset: 3 }}>
+          <Alert
               variant="success"
               show={showAlert}
               onClose={() => setShowAlert(false)}
               dismissible
             >
-              <Alert.Heading>Thanks, {formData?.name} updated</Alert.Heading>
-              <p>Thank you for making a booking with</p>
+              <Alert.Heading>Thanks, we've updated your information</Alert.Heading>
+              {/* <p>Thanks, we've updated your information</p> */}
             </Alert>
             <PageHeader
               title="Profile page"
-              description="Feel free to help us out around the place."
+              description="We use this information to populate your information accross the site"
             />
+   
             <Container className="d-flex align-items-center py-3 my-3 border-top border-bottom">
               <Image
                 className="profile-img"
@@ -90,6 +87,7 @@ function Resident() {
               <div>
                 <p className="data-title mb-0">Email</p>
                 <p>{user?.email}</p>
+                
               </div>
             </Container>
             <Container>
@@ -139,11 +137,17 @@ function Resident() {
                     name="contactDetails"
                     as="textarea"
                     rows={3}
-                    placeholder="Enter how you'd like to be contacted here"
+                    placeholder="Let us know how you would like to be contacted e.g. a phone number or email address"
                     onChange={handleChange}
                     defaultValue={formData?.contactDetails}
                   />
-                </Form.Group>
+                </Form.Group >
+                <Form.Group className="mb-3">
+
+                <Form.Text className="text-muted">
+     If you need to update your email, please contact us at support@humm.com
+    </Form.Text>
+    </Form.Group>
 
                 <Button variant="primary" type="submit">
                   Save changes
