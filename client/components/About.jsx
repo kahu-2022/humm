e7d2356b2f028c7f7079af8a47053217b46097fa
Courtney Brown <93187275+courtney-brown-dev-journey@users.noreply.github.com>
2022-03-02 11:25:47 +1300

    added outline of about us

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Container from 'react-bootstrap/Container'
+import { Container, Row, Col } from 'react-bootstrap'
 import Image from 'react-bootstrap/Image'
 
 function About() {
@@ -39,7 +39,44 @@ function About() {
       <p>Phone</p>
       <p>Email</p>
     </section>
+    <Container>
+      <h2>Meet the team behind humm</h2>
+    <Row>
+    <Col  xs={6} md={4}>
+      <h5>Hareton </h5>
+      <em> Product owner, co-Git lead, and co-scrum master</em>
+      <p>Hareton loves dying their hair, their friend Ivy and yoga.</p>
+    </Col>
+    <Col  xs={6} md={4}>
+      <h5>Rose </h5>
+      <em>Front end lead, advisor</em>
+      <p>Rose loves her mum, the colour pink and coffee. </p>
+    </Col>
+    <Col  xs={6} md={4}>
+    <h5>John </h5>
+    <em>Vibes lead</em>
+    <p>John loves Marvel, dinosaurs and Marvel.</p>
 
+    </Col>
+  </Row>
+
+  <Row>
+    <Col xs={6} md={4} >
+    <h5>Jad </h5>
+    <em> Back end lead </em>
+    <p>Jad loves the idea of getting a flame tattoo, his car not being stolen and caldereta.</p>
+    </Col>
+    <Col xs={6} md={4}>
+    <h5>Courtney </h5>
+    <em> Co-Git lead, and co-scrum master</em>
+    <p>Courtney loves colour co-ordinated outfits, potatoes and sit-coms.</p>
+    </Col>
+    <Col xs={6} md={4}> 
+    <h5>Enspiral Dev Academy </h5>
+    <em> Kāhu class of 2022</em>
+    </Col>
+  </Row>
+</Container>
     </Container>
     </>
   )
