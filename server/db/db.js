a45c4a28f43d372b45eae1715f427f106dcf054b
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-03-01 16:23:40 +1300

    initial try on sorting food based on date donated

--- server/db/db.js
+++ server/db/db.js
@@ -66,7 +66,7 @@ function addRoomIssues(issue, db = conn) {
 }
 
 function getFood(db = conn) {
-  return db('food-items').select()
+  return db('food-items').select().orderBy("donateDate")
 }
 
 function getFoodById(foodId, db = conn) {
