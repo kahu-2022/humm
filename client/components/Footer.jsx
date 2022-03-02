f12b6bc0dd34debde1601a4479f9276abfdd47e6
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-03-02 19:34:26 +1300

    refactor imports for Footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Button, Container, Row, Col, Image } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 
 function Footer() {
 
