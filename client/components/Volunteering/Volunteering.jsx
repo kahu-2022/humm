28e31d94cabccb8fc7727511623853a296c9a306
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-03-03 00:33:44 +1300

    refactor imports in volunteering pages

--- client/components/Volunteering/Volunteering.jsx
+++ client/components/Volunteering/Volunteering.jsx
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from "react"
-import {
-  Row,
-  Col,
-  Card,
-  Container,
-  Alert,
-  Modal,
-  Form,
-  Button,
-} from "react-bootstrap"
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
+import React, { useState, useEffect } from 'react'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
 
-import { signUpForVolunteering, getUserByEmail } from "../../apis/api"
+import { useAuth0 } from '@auth0/auth0-react'
 
-function Volunteering(props) {
+import { signUpForVolunteering, getUserByEmail } from '../../apis/api'
+
+function Volunteering (props) {
 
   const { volunteer } = props
   const { user } = useAuth0()
