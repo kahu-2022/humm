e4b8e0c2d945c706e2aac24720b3e672995737e8
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-03-02 23:49:10 +1300

    conditionally add login/register button based on auth, refactor imports

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Row, Col, Container, Button, Image } from 'react-bootstrap'
-import { withAuthenticationRequired } from "@auth0/auth0-react"
-import Loading from "./Loading"
-import Team from "./Team"
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
+
+import { useAuth0 } from "@auth0/auth0-react"
+
+
 
 function Home() {
+  const { isAuthenticated, loginWithRedirect } = useAuth0()
+
+  function handleRegister(e) {
+    e.preventDefault()
+    loginWithRedirect()
+  }
+
   return (
     <>
       <Container className="py-3">
@@ -14,13 +26,17 @@ function Home() {
           <h1 className="landing-hero">Welcome to humm!</h1>
           <h5>We’re a community dedicated to housing and feeding vulnerable members of our whānau, as well as providing them with resources and strategies to recover and grow.</h5>
           <br></br>
-        <Button variant="primary" className="mb-3">
-           Login
+        {
+        !isAuthenticated ?
+        <>
+        <Button variant="primary" className="mb-3" onClick={handleRegister}>
+           Login / Register
         </Button> <span></span>
-        <Button variant="primary" className="mb-3">
-           Register
+        </> : null } 
+        <Button href="/About" variant="primary" className="mb-3">
+           About the Project
         </Button> <span></span>
-        <Button href="./Team" variant="primary" className="mb-3">
+        <Button href="/Team" variant="primary" className="mb-3">
            Meet the team
         </Button>
         </header>
