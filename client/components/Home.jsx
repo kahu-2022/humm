52084b4846d50d841177f077908dcf92499f9085
rose-baylis <rose.l.baylis@gmail.com>
2022-03-02 16:11:32 +1300

    Made auth nav component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,38 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col, Container, Button, Image } from 'react-bootstrap'
+import React, { useState, useEffect } from "react"
+import { Row, Col, Container, Button, Image } from "react-bootstrap"
 import { withAuthenticationRequired } from "@auth0/auth0-react"
-import Loading from './Loading'
-
+import Loading from "./Loading"
 
 function Home() {
-  
-
   return (
     <>
       <Container className="py-3">
         <Row>
           <Col className="p-5" md={6}>
-          <header className="mt-4 header">
-          <h1 className="landing-hero">We’re a community dedicated to housing</h1>
-          <p>We’re a community dedicated to housing and feeding vulnerable members of our whānau, as well as providing them with resources and strategies to recover and grow.
-
-</p>
-<Button variant="primary" className="mb-3">
-           Login
-        </Button> <span></span>
-        <Button variant="primary" className="mb-3">
-           Register
-        </Button>
-        </header>
-        
+            <header className="mt-4 header">
+              <h1 className="landing-hero">
+                We’re a community dedicated to housing
+              </h1>
+              <p>
+                We’re a community dedicated to housing and feeding vulnerable
+                members of our whānau, as well as providing them with resources
+                and strategies to recover and grow.
+              </p>
+              <Button variant="primary" className="mb-3">
+                Login
+              </Button>{" "}
+              <span></span>
+              <Button variant="primary" className="mb-3">
+                Register
+              </Button>
+            </header>
           </Col>
           <Col>
-          <Image src="./images/vibes/building-cropped-2.jpeg" fluid="true" rounded="true" className="mt-2 mb-4" />
+            <Image
+              src="./images/vibes/building-cropped-2.jpeg"
+              fluid="true"
+              rounded="true"
+              className="mt-2 mb-4"
+            />
           </Col>
         </Row>
-    
-        
-      
+
         <br></br>
         {/* <Button href="/about" variant="primary" className="mb-3">
           Browse as a guest
--- client/components/Navigation/Navigation.jsx
+++ client/components/Navigation/Navigation.jsx
@@ -1,50 +1,19 @@
 import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
 import { Navbar, NavDropdown, Nav, Container, Button, Image } from "react-bootstrap"
 
-import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated"
+// import { IfAuthenticated, IfNotAuthenticated } from "./Authenticated"
 
 import { useAuth0 } from "@auth0/auth0-react"
+import AuthNav from "./AuthNav"
 
-import { useDispatch } from 'react-redux'
-import { fetchUser, setUser } from '../actions/user'
 
 
 function Navigation(props) {
 
-  const dispatch = useDispatch()
 
 
   const { loginWithRedirect, logout } = useAuth0()
-  const { user ,isAuthenticated} = useAuth0();
-
-
-  function handleLogoff(e) {
-    e.preventDefault()
-    logout({ returnTo: window.location.origin })
-  }
-
-  function handleRegister(e) {
-    e.preventDefault()
-    loginWithRedirect({
-      redirectUri: `${window.location.origin}/register`,
-    })
-  }
-
-  function handleSignIn(e) {
-    e.preventDefault()
-    loginWithRedirect()
-  }
-
-    
-  useEffect(() => {
-    if (isAuthenticated) {
-
-    console.log("trdyfug")
- //   dispatch(fetchUser(user))
-    }
-  }, [])
-
+  const { user ,isAuthenticated, isLoading} = useAuth0();
 
   return (
     <Navbar collapseOnSelect expand="lg" className="navigation" variant="dark">
@@ -55,7 +24,6 @@ function Navigation(props) {
           <Nav>
             <Nav.Link href="/about">About us</Nav.Link>
           </Nav>
-          <IfAuthenticated>
             <Nav className="me-auto">
               <NavDropdown title="Mental Health" id="collapsible-nav-dropdown">
                 <NavDropdown.Item href="/counsellors">
@@ -76,28 +44,12 @@ function Navigation(props) {
               <Nav.Link href="/room">Report an issue</Nav.Link>
             <Nav.Link href="/volunteering">Volunteering</Nav.Link>
             </Nav>
-            <Nav>
-            <Nav.Link href="/resident">
-              <Image
-                src={user?.picture}
-                title="my page"
-                fluid="true"
-                className="arrow"
-                roundedCircle={true} 
-              />
-              
-              </Nav.Link>
-            </Nav>
-          <Nav>
-            <Nav.Link onClick={handleLogoff}>Logout</Nav.Link>
-          </Nav>
-          </IfAuthenticated>          
+           
+
+            <AuthNav />
           
         </Navbar.Collapse>
 
-        <IfNotAuthenticated>
-          <Button variant="primary" onClick={handleSignIn}>Login</Button>
-        </IfNotAuthenticated>
       </Container>
     </Navbar>
   )
